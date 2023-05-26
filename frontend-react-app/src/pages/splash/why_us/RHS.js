8ef6b8b6e53e048597adbfaa8b0967d4d70ed857
Peter Driscoll <patardriscoll@gmail.com>
2023-05-26 11:06:11 -0400

    update

--- frontend-react-app/src/pages/splash/why_us/RHS.js
+++ frontend-react-app/src/pages/splash/why_us/RHS.js
@@ -5,23 +5,31 @@ import Stack from '@mui/material/Stack';
 import splashImage2 from '../../../static/images/splash_image_2_blue.png';
 import CalendlyButton from './CalendlyButton.js';
 import styles from './scss/WhyUs.module.scss';
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const RHS = () => (
-  <Grid container item lg={6} className={styles.rhsContainer}>
-    <Grid item xs={9}>
-      <CardMedia
-        src={splashImage2}
-        component="img"
-        alt="smiling lady"
-        className={styles.img}
-      ></CardMedia>
+const RHS = () => {
+  const navigate = useNavigate();
+  return (
+    <Grid container item lg={6} className={styles.rhsContainer}>
+      <Grid item xs={9}>
+        <CardMedia
+          src={splashImage2}
+          component="img"
+          alt="smiling lady"
+          className={styles.img}
+        ></CardMedia>
+      </Grid>
+      <Grid item>
+        <Stack className={styles.stack}>
+          <Typography>Questions?</Typography>
+          {/* <CalendlyButton></CalendlyButton> */}
+          <Button onClick={() => navigate('/faqs')} className={styles.button}>
+            Contact us
+          </Button>
+        </Stack>
+      </Grid>
     </Grid>
-    <Grid item>
-      <Stack className={styles.stack}>
-        <Typography>Questions?</Typography>
-        <CalendlyButton></CalendlyButton>
-      </Stack>
-    </Grid>
-  </Grid>
-);
+  );
+};
 export default RHS;
