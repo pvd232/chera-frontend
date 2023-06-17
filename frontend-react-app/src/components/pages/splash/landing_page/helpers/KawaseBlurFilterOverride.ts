9faf43e23a57a81c999e022163427240c50874f5
Peter Driscoll <patardriscoll@gmail.com>
2023-06-17 12:07:55 -0400

    removed babel preset.js and removed kawase blur

--- frontend-react-app/src/components/pages/splash/landing_page/helpers/KawaseBlurFilterOverride.ts
+++
@@ -1,7 +0,0 @@
-import { KawaseBlurFilter } from '@pixi/filter-kawase-blur';
-
-export default class KawaseOverride extends KawaseBlurFilter {
-    constructor(blur:number, quality:number, clamp:boolean) {
-      super(blur, quality, true);
-    }
-  }
