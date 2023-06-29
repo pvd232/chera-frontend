3d22aebe9d3e0abc1fbcf19d400d50bb26cff022
KUSHAGRA SETH <kushagraseth.1996@gmail.com>
2023-06-29 08:47:21 -0700

    remove unused dependency

--- frontend-react-app/src/components/pages/splash/Splash.js
+++ frontend-react-app/src/components/pages/splash/Splash.js
@@ -3,7 +3,6 @@ import HowItWorks from './how_it_works/HowItWorks';
 import About from './About';
 import WhyUs from './why_us/WhyUs';
 import LandingPage from './landing_page/LandingPage.tsx';
-import MissionPage from './mission_page/MissionPage';
 import FNCEApproved from './FNCEApproved';
 import Footer from './Footer';
 import styles from './scss/Splash.module.scss';
